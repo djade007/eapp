ea8ba6265cffaf56c09dfe8d4a21795bb2322b5b
Olajide Afeez <jide4real43@gmail.com>
2021-03-13 21:35:39 +0100

    Update default time

--- EQMobileWork/src/main/java/com/example/eqmobilework/data/LocationEvent.kt
+++ EQMobileWork/src/main/java/com/example/eqmobilework/data/LocationEvent.kt
@@ -3,6 +3,6 @@ package com.example.eqmobilework.data
 data class LocationEvent(
     val lat: Float,
     val lon: Float,
-    val time: Long = System.currentTimeMillis(),
+    val time: Long = System.currentTimeMillis() / 1000L,
     val ext: String,
 )
